2f6ce683fb6544e9d89003be25e05f30e650e696
brnnoliv1972-dotcom <brnnoliv1972@gmail.com>
2026-08-26 14:57:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 import requests
 from flask import Flask, request, jsonify
@@ -40,3 +39,4 @@ def send_zapi_message(phone, message):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
